6027c5a1a22834547463c9a07bc0a1032476ded5
Ashwin Seshadri <oklash@hotmail.com>
2023-10-14 22:52:19 -0700

    moved settings to environment variables

--- src/database/config.js
+++ src/database/config.js
@@ -6,14 +6,19 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCt3yqSjrMPuEr_6bs5Et2_enwVwEJ-A1w",
-  authDomain: "photowall-1b0f0.firebaseapp.com",
-  databaseURL: "https://photowall-1b0f0-default-rtdb.firebaseio.com",
-  projectId: "photowall-1b0f0",
-  storageBucket: "photowall-1b0f0.appspot.com",
-  messagingSenderId: "436838781021",
-  appId: "1:436838781021:web:c0e1e61a4acd162a3bfdb4"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
+console.log(process.env);
+console.log(process.env.NODE_ENV);
+console.log(process.env.REACT_APP_API_KEY);
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
